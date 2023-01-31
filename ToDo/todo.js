90773dd1c2237e3a314202e16b6bf409ac3d1cf7
nichtmax06 <122627896+nichtmax06@users.noreply.github.com>
2023-02-01 00:14:04 +0100

    tet

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -1,62 +1,153 @@
-window.addEventListener('load', () => {
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
+// todo.js
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
+// Get the form element
+const taskForm = document.getElementById('new-task-form');
+// Get the task list element
+const tasksElement = document.getElementById('tasks');
 
-		const task = input.value;
+// Event listener for the form
+taskForm.addEventListener('submit', function(e) {
+	e.preventDefault();
 
-		const task_el = document.createElement('div');
-		task_el.classList.add('task');
+	// Get the value of the input element
+	const taskInput = document.getElementById('new-task-input');
+	const taskInputValue = taskInput.value;
 
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
+	if (taskInputValue === '') {
+		alert('Please enter a valid task');
+		return;
+	}
 
-		task_el.appendChild(task_content_el);
+	// Create a new task
+	const task = document.createElement('div');
+	task.classList.add('task');
+	task.innerHTML = `
+		<div class="content">
+			<input 
+				type="text" 
+				class="text" 
+				value="${taskInputValue}"
+				readonly>
+		</div>
+		<div class="actions">
+			<button class="edit">Edit</button>
+			<button class="delete">Delete</button>
+		</div>
+	`;
 
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
+	// Append the task to the task list
+	tasksElement.appendChild(task);
 
-		task_content_el.appendChild(task_input_el);
+	// Add the task to localStorage
+	addTaskToLocalStorage(taskInputValue);
 
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
-
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
-
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
-
-		task_el.appendChild(task_actions_el);
-
-		list_el.appendChild(task_el);
-
-		input.value = '';
-
-		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-			}
-		});
-
-		task_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(task_el);
-		});
-	});
+	// Reset the input value
+	taskInput.value = '';
 });
+
+// Event listener for the edit button
+tasksElement.addEventListener('click', function(e) {
+	if (e.target.classList.contains('edit')) {
+		// Get the task text
+		const taskText = e.target.parentElement.previousElementSibling.children[0];
+		// Make the text editable
+		taskText.removeAttribute('readonly');
+		// Change the edit button text
+		e.target.innerText = 'Save';
+		// Change the class to save
+		e.target.classList.replace('edit', 'save');
+	}
+
+	if (e.target.classList.contains('save')) {
+		// Get the task text
+		const taskText = e.target.parentElement.previousElementSibling.children[0];
+		// Make the text non-editable
+		taskText.setAttribute('readonly', 'readonly');
+		// Change the save button text
+		e.target.innerText = 'Edit';
+		// Change the class to edit
+		e.target.classList.replace('save', 'edit');
+		// Update local storage
+		updateLocalStorage(taskText.value);
+	}
+
+	if (e.target.classList.contains('delete')) {
+		// Get the task
+		const task = e.target.parentElement.parentElement;
+		// Remove the task from the DOM
+		tasksElement.removeChild(task);
+		// Remove the task from local storage
+		removeTaskFromLocalStorage(task);
+	}
+});
+
+// Function to add a task to local storage
+function addTaskToLocalStorage(task) {
+	let tasks;
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+	tasks.push(task);
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to update a task in local storage
+function updateLocalStorage(task) {
+	let tasks = JSON.parse(localStorage.getItem('tasks'));
+	// Find the index of the task
+	const taskIndex = tasks.indexOf(task);
+	// Update the task in the tasks array
+	tasks[taskIndex] = task;
+	// Update local storage
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to remove a task from local storage
+function removeTaskFromLocalStorage(task) {
+	let tasks = JSON.parse(localStorage.getItem('tasks'));
+	// Get the task text
+	const taskText = task.children[0].children[0].value;
+	// Find the index of the task
+	const taskIndex = tasks.indexOf(taskText);
+	// Remove the task from the array
+	tasks.splice(taskIndex, 1);
+	// Update local storage
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to load up tasks when page is refreshed
+function loadTasks() {
+	let tasks;
+	if (localStorage.getItem('tasks') === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+
+	tasks.forEach(function(task) {
+		// Create a new task
+		const taskElement = document.createElement('div');
+		taskElement.classList.add('task');
+		taskElement.innerHTML = `
+			<div class="content">
+				<input 
+					type="text" 
+					class="text" 
+					value="${task}"
+					readonly>
+			</div>
+			<div class="actions">
+				<button class="edit">Edit</button>
+				<button class="delete">Delete</button>
+			</div>
+		`;
+
+		// Append the task to the task list
+		tasksElement.appendChild(taskElement);
+	});
+}
+
+// Load up tasks when page is refreshed
+loadTasks();
